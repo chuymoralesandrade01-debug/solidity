1d07d21095a188efdca607217ce0437fc10e16c2
rodiazet <radek.zagorowicz@gmail.com>
2026-01-14 13:51:32 +0100

    Add better coverage for testing function selector purity.

--- test/libsolidity/syntaxTests/types/functionTypes/selector/event_error_selector_pure_assignment.sol
+++ test/libsolidity/syntaxTests/types/functionTypes/selector/event_error_selector_pure_assignment.sol
@@ -1,8 +1,27 @@
+==== Source: A.sol ====
+
+event EvAExt();
+error ErAExt();
+
+contract A {
+    event Ev();
+    error Er();
+}
+
+==== Source: C.sol ====
+import * as AMod from "A.sol";
+
 event EvExt();
 error ErExt();
 
 bytes32 constant eventExtSelectorGlobal = EvExt.selector;
 bytes4 constant errorExtSelectorGlobal = ErExt.selector;
+
+bytes32 constant eventAExtSelectorGlobal = AMod.EvAExt.selector;
+bytes4 constant errorAExtSelectorGlobal = AMod.ErAExt.selector;
+
+bytes32 constant eventASelectorGlobal = AMod.A.Ev.selector;
+bytes4 constant errorASelectorGlobal = AMod.A.Er.selector;
 
 contract C {
     event Ev();
@@ -16,5 +35,11 @@ contract C {
 
     bytes4 constant errorSelectorC = C.Er.selector;
     bytes32 constant eventSelectorC = C.Ev.selector;
+
+    bytes32 constant eventAExtSelector = AMod.EvAExt.selector;
+    bytes4 constant errorAExtSelector = AMod.ErAExt.selector;
+
+    bytes32 constant eventASelector = AMod.A.Ev.selector;
+    bytes4 constant errorASelector = AMod.A.Er.selector;
 }
 // ----
